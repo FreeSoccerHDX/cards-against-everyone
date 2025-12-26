c240b4ce21d23e85811e92a9a1ed19f9b3425eef
FreeSoccerHDX <freesoccerhdx@gmail.com>
2025-12-26 22:11:34 +0100

    add game.py

--- game.py
+++ game.py
@@ -14,13 +14,13 @@ class Game:
         # Game variables
         self.history = []          # list of past rounds
         self.current_round = 0     # current round number
-        self.playerCards = {}   # player_id: list of white cards
-        self.current_black_card = "" # current black card -> string
+        self.playerCards = {}   # playerName: list of white cards
+        self.current_black_card = {} # current black card -> card_text: str, num_blanks: int
         self.winning_white_cards = []  # list of winning white cards in current round
-        self.submitted_white_cards = {}  # player_id: [white_card(s)]
-        self.scores = {}           # player_id: score
+        self.submitted_white_cards = {}  # playerName: [white_card(s)]
+        self.scores = {}           # playerName: score
         self.czarIndex = 0      # index of current czar in active_players -> random set at start
-        self.czar = None           # player_id of current czar
+        self.czar = None           # playerName of current czar
         self.state = 'lobby'  # 'lobby', 'choosing_cards', 'choosing_winner', 'countdown_next_round', 'game_ended'
         self.currentTimerTotalSeconds = 0  # total seconds for current timer
         self.currentTimerSeconds = 0      # remaining seconds for current timer
@@ -39,10 +39,24 @@ class Game:
         }
     
     def timer_tick(self):
-        if self.currentTimerSeconds >= 0:
-            self.currentTimerSeconds -= 1
-            return True
-        return False
+        if self.currentTimerSeconds < 0:
+            return False
+        
+        self.currentTimerSeconds -= 1
+        
+        if self.currentTimerSeconds == 0:
+            if self.state == 'choosing_cards':
+                self.autosubmit_white_cards()
+            if self.state == 'choosing_winner':
+                # auto choose random winner
+                possible_winners = list(self.submitted_white_cards.keys())
+                if possible_winners:
+                    chosen_winner = random.choice(possible_winners)
+                    self.choose_winner(chosen_winner, choosing_playerName=None)
+            if self.state == 'countdown_next_round':
+                self.next_round()
+
+        return True
 
     def is_game_started(self):
         return self.state != 'lobby'
@@ -125,17 +139,94 @@ class Game:
                 new_card = random.choice(CARDS_ANSWERS)
                 self.playerCards[player].append(new_card)
 
+    def autosubmit_white_cards(self):
+        for playerName in self.active_players:
+            if playerName != self.czar and playerName not in self.submitted_white_cards:
+                # choose random white card(s)
+                num_cards_needed = self.current_black_card["num_blanks"]
+                player_hand = self.playerCards[playerName]
+                chosen_cards = random.sample(player_hand, num_cards_needed)
+                self.submit_white_card(playerName, chosen_cards)
+
+    def submit_white_card(self, playerName, white_cards):
+        if self.state != 'choosing_cards':
+            return False
+        if playerName == self.czar:
+            return False
+        if playerName not in self.active_players:
+            return False
+        if playerName in self.submitted_white_cards:
+            return False
+    
+        self.submitted_white_cards[playerName] = white_cards
+        # remove white cards from player's hand
+        playerCards = self.playerCards.get(playerName, [])
+        for card in white_cards:
+            if card in playerCards:
+                playerCards.remove(card)
+        self.playerCards[playerName] = playerCards
+
+        # if everyone has submitted, move to choosing_winner
+        if len(self.submitted_white_cards) >= len(self.active_players) - 1:
+            self.state = 'choosing_winner'
+            self.currentTimerTotalSeconds = self.settings["timeToChooseWinner"]
+            self.currentTimerSeconds = self.currentTimerTotalSeconds
+
+        return True
+
+    def choose_winner(self, winner_playerName, choosing_playerName=None):
+        if self.state != 'choosing_winner':
+            return False
+        if choosing_playerName != self.czar and choosing_playerName != None:
+            return False
+        if winner_playerName not in self.submitted_white_cards:
+            return False
+
+        winning_cards = self.submitted_white_cards[winner_playerName]
+        self.winning_white_cards = winning_cards
+        self.scores[winner_playerName] += 1
+        self.history.append({
+            'round': self.current_round,
+            'black_card': self.current_black_card,
+            'submitted_cards': self.submitted_white_cards,
+            'winner': winner_playerName,
+            'winning_cards': winning_cards,
+            'czar': choosing_playerName # can be None if auto-chosen
+        })
+
+        self.state = 'countdown_next_round'
+        self.currentTimerTotalSeconds = self.settings["timeAfterWinnerChosen"]
+        self.currentTimerSeconds = self.currentTimerTotalSeconds
+        
+
     def next_round(self):
-        pass
+        if self.state != 'countdown_next_round':
+            return False
+        
+        # check if max rounds reached
+        if (self.current_round+1) >= self.settings["maxRounds"]:
+            self.end_game()
+            return True
+        
+        # check if player reached score to win
+        for player, score in self.scores.items():
+            if score >= self.settings["maxPointsToWin"]:
+                self.end_game()
+                return True
 
-    def submit_white_card(self, player, white_card):
-        pass
-
-    def choose_winner(self, winner_player):
-        pass
+        self.current_round += 1
+        self.submitted_white_cards = {}
+        self.winning_white_cards = []
+        self.czarIndex = (self.czarIndex + 1) % len(self.active_players)
+        self.czar = self.active_players[self.czarIndex % len(self.active_players)]
+        self.current_black_card = random.choice(CARDS_QUESTIONS)
+        self.fill_player_hands()
+        self.state = 'choosing_cards'
+        self.currentTimerTotalSeconds = self.settings["timeToChooseWhiteCards"]
+        self.currentTimerSeconds = self.currentTimerTotalSeconds
 
     def end_game(self):
-        pass
-
-    def get_winner(self):
-        pass
+        self.state = 'game_ended'
+        self.currentTimerTotalSeconds = -1
+        self.currentTimerSeconds = -1
+        
